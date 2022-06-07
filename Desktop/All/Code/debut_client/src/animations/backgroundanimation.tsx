36cc43a9a691410cb01b298e304b1775c2613c90
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-06-07 00:29:37 -0400

    background anomatin

---
+++ Desktop/All/Code/debut_client/src/animations/backgroundanimation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Styles/homeBackground.css'
+export default function Backgroundanimation() {
+    return (
+        <div className='ripple-background'>
+            <div className='circle xxlarge shade1'></div>
+            <div className='circle xlarge shade2'></div>
+            <div className='circle large shade3'></div>
+            <div className='circle mediun shade4'></div>
+            <div className='circle small shade5'></div>
+            <div className='circle small shade6'></div>
+            <div className='circle small shade7'></div>
+        </div>
+    )
+}
