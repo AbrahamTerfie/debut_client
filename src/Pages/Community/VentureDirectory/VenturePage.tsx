4008701d8b5341ef70e4e32b18ee409db72936ec
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-06-27 23:55:53 -0400

    ...

--- src/Pages/Community/VentureDirectory/VenturePage.tsx
+++ src/Pages/Community/VentureDirectory/VenturePage.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Row, Col } from 'reactstrap'
 import { useParams } from 'react-router-dom'
+import './VenturePage.css'
 export default function VenturePage() {
     const { id } = useParams()
+
     return (
-        <div style={{ marginTop: '100px', }}>
-            venture page if this id is
-            <br />
+        <>
+            <Row className='ventureProfileHeader m-3 w-100 h-100 '>
 
-            {id}
+                <div>
+                    <img src='https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='user profile photo' />
+                </div>
 
-            <br />
+            </Row>
+        </>
 
-
-        </div>
     )
 }
