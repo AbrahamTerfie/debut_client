a925e91a7bd3243f24eab4bbf3ed11c55c25b5ee
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-10-03 23:38:25 -0400

    started to look good

--- src/Components/LandingCard/LandingCard.tsx
+++ src/Components/LandingCard/LandingCard.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import '../../Styles/Landing.css'
 import { FaHandsHelping } from 'react-icons/fa'
-
+import { motion } from "framer-motion";
 // helop , point , gratitide
 export default function LandingCard({ icon, title }:
     { icon: any, title: string }) {
     return (
-        <div className=" border 
+        <motion.div className=" border 
             d-flex justify-content-center align-items-center
             flex-column p-3 mx-2 my-5
             shadow-sm rounded
             w-25 h-50
-        "    >
+        "
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        >
             <div
                 className='my-4'
             >
@@ -25,7 +28,7 @@ export default function LandingCard({ icon, title }:
                     {title}
                 </p>
             </div>
-        </div>
+        </motion.div>
 
     )
 }
--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -7,6 +7,9 @@ import { FaHandsHelping, FaHandPointUp, FaHandHoldingHeart } from 'react-icons/f
 import { VscDebugStart } from 'react-icons/vsc'
 import { Link } from 'react-router-dom'
 import { appRoutes } from '../../Routes/routes'
+import LandingScroll from '../../animations/Framer/LandingScroll'
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+
 const iconProperties = {
     size: 40,
     color: '#87ceeb'
@@ -16,70 +19,116 @@ const iconProperties = {
 
 export default function Landing() {
     return (
-        <Container fluid className="p-4" >
-            <>
-                <Row className='headerText'  >
-                    Debut
-                </Row>
-                <Container fluid sm={12} md="12"   >
-                    <Row
-                        className='d-flex justify-content-center align-items-center'
-                    >
-                        <Col md={5}
-                            className="shadow-sm  p-5 rounded mx-2"
-                        >
-                            <p
-                                className='fs-6 fw-lighter text-start'>
-                                Debut is a digital platform aimed at helping founders mobilize
-                                their village (family, friends and extended network) to help launch their startup or project.
-                                Think “baby-shower for your startup”.
-                            </p>
 
-                        </Col>
-                        <Col md={5} className="
-                            shadow-sm  p-5 rounded  forumCardParent mx-2"
-                        >
-                            <Link to={appRoutes.authentication}>
-                                <div className=" p-5 "   >
-                                    <p className='m-0' >
+
+        <Container fluid className="p-4" >
+
+            <div>
+                <Parallax pages={3} style={{ top: '0', left: '0' }}>
+                    <ParallaxLayer
+                        offset={0}
+                        speed={1.5}
+                        className='d-flex justify-content-start align-items-center
+                        flex-column p-3 mx-2 my-5
+                        '
+                    >
+
+
+                        <Row className='headerText'  >
+                            <p>Debut</p>
+                        </Row>
+                        <p className='fs-4 m-0' >Scroll down</p>
+                        <p className='text-muted m-0' > to learn more </p>
+
+                    </ParallaxLayer>
+
+                    {/* <ParallaxLayer offset={1} speed={1} /> */}
+
+                    <ParallaxLayer
+
+                        offset={0.5}
+                        speed={0.5}
+                        style={{
+                            backgroundColor: '#A2BCE0',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            color: 'lightblue',
+                        }}>
+                        <Row className='d-flex justify-content-center align-items-center'>
+                            <Col md={5} className="shadow-sm  p-5 rounded mx-2 border border-light" >
+                                <p
+                                    className='fs-6 fw-lighter text-start text-light '>
+                                    Debut is a digital platform aimed at helping founders mobilize
+                                    their village (family, friends and extended network) to help launch their startup or project.
+                                    Think “baby-shower for your startup”.
+                                </p>
+
+                            </Col>
+                            <Col md={5} className="shadow-sm  p-5 rounded  forumCardParent mx-2" >
+                                <Link to={appRoutes.authentication}>
+                                    <div className=" p-4 d-flex justify-content-center align-items-center ">
                                         <VscDebugStart
                                             className='mx-3'
-                                            size={20}
-                                            color='#87ceeb'
+                                            size={30}
+                                            color='#87ceeb' />
+                                        <div>
+                                            <p className='m-0 fs-3' >get started </p>
+                                            <span className='text-muted' >
+                                                sign up or login
+                                            </span>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </Col>
+                        </Row>
 
-                                        />
-                                        get started
-                                    </p>
-                                </div>
-                            </Link>
-                        </Col>
-                    </Row>
-
-
-                </Container  >
-                <div className='landing-container' >
-
-                    <LandingCard
-                        title='ask for help'
-                        icon={<FaHandsHelping
-                            size={iconProperties.size}
-                            color={iconProperties.color}
-                        />} />
-
-                    <LandingCard
-                        title='frictionless help'
-                        icon={<FaHandPointUp size={iconProperties.size}
-                            color={iconProperties.color} />} />
-
-                    <LandingCard
-                        title='follow up with gratitude'
-                        icon={<FaHandHoldingHeart size={iconProperties.size}
-                            color={iconProperties.color} />} />
+                        <p>Scroll up</p>
+                    </ParallaxLayer>
+                    {/* <ParallaxLayer offset={2} speed={2} style={{  }} /> */}
 
 
+                    <ParallaxLayer
+                        offset={1}
+                        speed={0.5}
+                        style={{
+                            backgroundColor: 'lightgray',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            color: 'lightblue',
+                        }}>
+                        <div className='landing-container' >
 
-                </div>
-            </>
+                            <LandingCard
+                                title='ask for help'
+                                icon={<FaHandsHelping
+                                    size={iconProperties.size}
+                                    color={iconProperties.color}
+                                />} />
+
+                            <LandingCard
+                                title='frictionless help'
+                                icon={<FaHandPointUp size={iconProperties.size}
+                                    color={iconProperties.color} />} />
+
+                            <LandingCard
+                                title='follow up with gratitude'
+                                icon={<FaHandHoldingHeart size={iconProperties.size}
+                                    color={iconProperties.color} />} />
+
+
+
+                        </div>
+
+                        <p>Scroll up</p>
+                    </ParallaxLayer>
+
+
+                </Parallax>
+
+            </div>
+
 
         </Container>
     )
