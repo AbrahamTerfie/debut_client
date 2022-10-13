689891cc7647900cacc1cc4992f04af4e1fa7a75
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-10-13 18:25:19 -0400

    feat : create new goal form

---
+++ src/Components/DashBoard/NewGoalForm/NewGoalForm.tsx
@@ -0,0 +1,32 @@
+
+
+
+import React, { useState } from 'react';
+import { Row, Col, FormGroup, Input, Label } from 'reactstrap';
+
+export default function NewGoalForm(
+) {
+
+    return (
+        <div>
+            {/* name input  */}
+            <Row>
+                <Col md={12} >
+                    <FormGroup>
+                        <Label for="exampleEmail">  title </Label>
+                        <Input type="text" name="goalTitle" id="goalTitle" placeholder=" name your goal" />
+                    </FormGroup>
+                </Col>
+                <Col md={12} >
+                    <FormGroup>
+                        <Label for="goalDescription">  description </Label>
+                        <Input type="textarea"
+                            col={9}
+                            name="goalDescription" id="goalDescription" placeholder=" describe your goal in deatil" />
+                    </FormGroup>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
--- src/Pages/DashBoard/DashBoardPages/CompanyGoals.tsx
+++ src/Pages/DashBoard/DashBoardPages/CompanyGoals.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Button, Col } from 'reactstrap';
-import GoalsAccordion from '../../../Components/DashBoard/GoalsAccordion';
-
+import { Button, Col, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import GoalsAccordion from '../../../Components/DashBoard/GoalsAccordion/GoalsAccordion';
+import NewGoalForm from '../../../Components/DashBoard/NewGoalForm/NewGoalForm';
 
 
 
@@ -85,17 +85,40 @@ function AccordionBody() {
 
 export default function CompanyGolas(props: any) {
 
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div>
       <div className='d-flex justify-content-between align-items-center'>
-
         <div className='mx-5'>
           <p className=' fs-1 fw-light mt-4 mb-1 mx-3' > company goals </p>
           <p className='text-muted fs-6 mt-0 mb-2 mx-3'>  once  your complete  it will be marked as achivement </p>
         </div>
+        <Button className=' mx-5' outline color='success' onClick={toggle}>
+          add new goal
+        </Button>
+        <Modal isOpen={modal} toggle={toggle} size="lg"  >
+          <ModalHeader toggle={toggle}>
+            <p className="fw-light m-0 fs-3">
+              create new goal for your company
+            </p>
+          </ModalHeader>
+          <ModalBody>
+            <NewGoalForm />
+          </ModalBody>
+          <ModalFooter>
+            <Button size="sm" color="secondary" outline className='px-4' onClick={toggle}>
+              Cancel
+            </Button>
+            <Button size="sm" color="success" outline className='px-4' onClick={toggle}>
+              create
+            </Button>{' '}
 
-        <Button className=' mx-5' outline color='success' > add new goal </Button>
-
+          </ModalFooter>
+        </Modal>
       </div>
 
       <div className='mx-3 p-3' >
