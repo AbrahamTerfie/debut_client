1863c41b7ee6c8276c10dff13d12b5165cb571aa
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-12-21 00:42:25 -0500

    ....

--- src/Auth/Auth0Provider.tsx
+++ src/Auth/Auth0Provider.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Auth0Provider } from '@auth0/auth0-react';
-import { Auth0Variables } from '../env';
+import { Auth0Variables } from '../environment';
 
 export const Auth0ProviderWithHistory = ({ children }:
     { children: any }) => {
---
+++ src/environment.ts
@@ -0,0 +1,5 @@
+export const Auth0Variables = {
+    REACT_APP_AUTH0_DOMAIN: "dev-4022i1o5.us.auth0.com",
+    REACT_APP_AUTH0_CLIENT_ID: "V6mKl4VbEllbpyPt4uQBGvLK7KgKdUpe",
+    REACT_APP_AUTH0_AUDIENCE: "https://debutAuthenticator"
+};
