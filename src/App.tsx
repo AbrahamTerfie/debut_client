5d63f2feebbb102abfff1b2c0c9c78022700c6cc
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-12-26 18:42:14 -0500

    new branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,12 @@ import './App.css';
 import "animate.css/animate.min.css";
 // import { Landing, Dashboard, DashBoardPages } from './Pages/inedx';
 // import { GiveGratitude, Forum, People, Ventures, CompanyDetailPage, Events, EventPage } from './Pages/Community/index';
-
-
 import { Routes, Route } from "react-router-dom";
 import { appRoutes } from './Routes/routes';
 import NavBarComponent from './Components/NavBar/NavBar';
 
 import { useAuth0 } from '@auth0/auth0-react';
 import { Toaster } from 'react-hot-toast';
-// import * as pages from './Pages/inedx'
-// import * as communityPages from './Pages/Community/index';
-// import * as DashBoardPages from './Pages/DashBoard/DashBoardPages/index';
 
 const Landing = React.lazy(() => import('./Pages/inedx').then((module) => ({ default: module.Landing })));
 const Dashboard = React.lazy(() => import('./Pages/inedx').then((module) => ({ default: module.Dashboard })));
